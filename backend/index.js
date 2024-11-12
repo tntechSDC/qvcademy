0b7a26311c5e2377ae54f6002da19dc0f9365596
Quinn Morrison <96103133+sqmorrison@users.noreply.github.com>
2024-11-11 21:44:30 -0600

    backend set up and installation of concurrently

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log('Listening on ${PORT}');
+})
