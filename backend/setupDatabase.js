9a3a6bdd9fd562021e346574bc98ddd1d2c23fdf
Quinn Morrison <96103133+sqmorrison@users.noreply.github.com>
2024-11-13 15:54:58 -0600

    installation of sqlite and postgress, added a connection to the backend and set up sqlite3

---
+++ backend/setupDatabase.js
@@ -0,0 +1,17 @@
+//this file will be used to set up the database located in the database.db file
+
+const sqlite3 = require('splite3').verbose();
+const db = new sqlite3.Database('./database.db');
+
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS tblUsers (
+    UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Username TEXT UNIQUE NOT NULL,
+    Email TEXT UNIQUE NOT NULL,
+    Password TEXT NOT NULL
+  )`);
+  console.log("User table created or already exists.");
+});
+
+
+db.close();
