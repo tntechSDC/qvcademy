e8b081a952a3d0f5e1b882a3d0b6168461b40cba
Cydney Miller <cydbags15@gmail.com>
2024-11-16 00:42:26 -0500

    Background for learningLandingPage
    added an image to be used as the background of the landing page, and added code to show on site

--- website/src/mainPages/learningLandingPage.js
+++ website/src/mainPages/learningLandingPage.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import LearningNavbar from '../components/learningNav';
+import background from "../images/LearningLandingPage_background.jpg";
 //this is going to be worked on by cyd
 
 export default function LearningHome() {
-    return (
+    return <div
+        style={{
+            backgroundImage: `url(${background})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "600vh",
+        }}
+    >
+        <LearningNavbar />
         <div>
-            <LearningNavbar />
             <h1 className="mt-16">This is the learning page</h1>
         </div>
-        
-    )
+    </div>
 }
