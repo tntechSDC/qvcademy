78d19f0b242fe4d0a3589b33c94751457c9d2292
Quinn Morrison <96103133+sqmorrison@users.noreply.github.com>
2024-11-17 21:31:15 -0600

    Learning Landing Page side nav added

--- website/src/components/learningNav.js
+++ website/src/components/learningNav.js
@@ -25,7 +25,7 @@ export default function LearningNavbar() {
     }, []);
 
     const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
-    return <nav className="bg-purple fixed top-0 w-full">
+    return <nav className="fixed top-0 bg-purple bg-opacity-40 w-full">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between space-x-4">   
               {/* Links on the left */}
@@ -37,32 +37,7 @@ export default function LearningNavbar() {
                 <NavLink to="/get-started" className="text-pink hover:bg-dark-purple px-3 py-2 rounded">Get Started</NavLink>
                 <NavLink to="/problems" className="text-pink hover:bg-dark-purple px-3 py-2 rounded">Program</NavLink>
                 <NavLink to="/learning" className="text-pink hover:bg-dark-purple px-3 py-2 rounded">Learn</NavLink>
-                <NavLink to="/" className="text-pink hover:bg-dark-purple px-3 py-2 rounded">Contact Us</NavLink>
-                    <div className="relative" ref={dropdownRef}>
-                        <button
-                            onMouseEnter={toggleDropdown}
-                            className="text-pink pt-2 hover:bg-dark-purple"
-                        >
-                            Languages &#x25BC;
-                        </button>
-
-                        {/* Dropdown Items */}
-                        {dropdownOpen && (
-                            <div className="absolute left-1/2 transform -translate-x-1/2 mt-2 bg-purple w-32 rounded-md">
-                                <NavLink to="/cppHome" className="block px-6 py-2 text-pink hover:bg-dark-purple rounded-md">C++</NavLink>
-                                <NavLink to="/get-started" className="block px-6 py-2 text-pink hover:bg-dark-purple rounded-md">C</NavLink>
-                                <NavLink to="/get-started" className="block px-6 py-2 text-pink hover:bg-dark-purple rounded-md">C#</NavLink>
-                                <NavLink to="/get-started" className="block px-6 py-2 text-pink hover:bg-dark-purple rounded-md">Swift</NavLink>
-                                <NavLink to="/get-started" className="block px-6 py-2 text-pink hover:bg-dark-purple rounded-md">Kotlin</NavLink>
-                                <NavLink to="/get-started" className="block px-6 py-2 text-pink hover:bg-dark-purple rounded-md">Lua</NavLink>
-                                <NavLink to="/get-started" className="block px-6 py-2 text-pink hover:bg-dark-purple rounded-md">Python</NavLink>
-                                <NavLink to="/get-started" className="block px-6 py-2 text-pink hover:bg-dark-purple rounded-md">Javascript</NavLink>
-                                <NavLink to="/get-started" className="block px-6 py-2 text-pink hover:bg-dark-purple rounded-md">Java</NavLink>
-                                <NavLink to="/get-started" className="block px-6 py-2 text-pink hover:bg-dark-purple rounded-md">Ruby</NavLink>
-                                <NavLink to="/get-started" className="block px-6 py-2 text-pink hover:bg-dark-purple rounded-md">Dart</NavLink>
-                            </div>
-                        )}
-                    </div>
+                <NavLink to="/" className="text-pink hover:bg-dark-purple px-3 py-2 rounded">Donate</NavLink>
           </div>
           <NavLink to="/login" className="text-pink hover:bg-dark-purple px-3 py-2 rounded">
             <UserCircleIcon className="h-10 w-10 text-pink hover:text-gray-400 cursor-pointer" />
--- website/src/mainPages/learningLandingPage.js
+++ website/src/mainPages/learningLandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import LearningNavbar from '../components/learningNav';
 import background from "../images/LearningLandingPage_background.jpg";
+import SideNav from '../components/learningSideNavs';
 //this is going to be worked on by cyd
 
 export default function LearningHome() {
@@ -13,9 +14,14 @@ export default function LearningHome() {
         }}
     >
         <LearningNavbar />
-        <div className="text-left pl-16 md:pl-36 pt-16">
-            <h1 className="mt-16 text-teal text-8xl md:text-8xl font-sans">Welcome!</h1>
-            <h2 className="text-pink text-2xl md:text-5xl font-sans font-light pt-8">Try Something New</h2>
+        <div className="flex">
+            <div className="mt-16">
+                <SideNav />
+            </div>
+            <div className="text-left pl-32 md:pl-48 pt-16">
+                <h1 className="mt-16 text-teal text-8xl md:text-8xl font-sans">Welcome!</h1>
+                <h2 className="text-pink text-2xl md:text-5xl font-sans font-light pt-8">Try Something New</h2>
+            </div>
         </div>
         {/* I am struggling to get the "Have Fun!" text at the bottom of the screen (at least on my display)*/}
         <div className="flex flex-col justify-end h-80"> {/*Searched this up to try, but only goes so far down*/ }
