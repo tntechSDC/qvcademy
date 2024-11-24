40b217b7ce8ae4e30e22314d71c96e0e46e466fe
Cydney Miller <cydbags15@gmail.com>
2024-11-23 21:51:29 -0500

    Added purple background to gettingStartedPage.js

--- website/src/mainPages/gettingStartedPage.js
+++ website/src/mainPages/gettingStartedPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import FunctionalNavbar from '../components/navbarDefault';
+//This is being worked on by Cyd
 
 export default function GettingStarted() {
     return (
-        <FunctionalNavbar />
-        
-    )
+        <div className="bg-purple h-screen flex">
+            <div className="text-left mt-16 pl-30">
+                <h1 className="text-pink">Welcome</h1>
+            </div>
+            <FunctionalNavbar />
+        </div>
+    );
 }
